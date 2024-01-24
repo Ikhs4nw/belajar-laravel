5e7979ea778f73bb5d8611fa35488226fbc06c3a
Ikhsan Wanugrah <ikhsanwanugrah93@gmail.com>
2024-01-25 00:09:58 +0700

    Pemisahan Class Dan Model Pada File CastController.php

--- app/Http/Controllers/CastController.php
+++ app/Http/Controllers/CastController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Cast;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 //return type View
 use Illuminate\View\View;
@@ -15,34 +16,38 @@ class CastController extends Controller
      */
     public function index()
     {
-        $cast = Cast::all();
-        return view('cast.vindex', ['title' => 'Data Cast'], compact('cast'));
+        // $cast = Cast::all();
+        $data_cast = Cast::getCast();
+        return view('cast.vindex', ['title' => 'Data Cast'], compact('data_cast'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store()
     {
-        $cast = new Cast;
-        $cast->nama = $request->input('name_cast');
-        $cast->umur = $request->input('age_cast');
-        $cast->bio = $request->input('bio_cast');
-        $cast->save();
+        // $cast = new Cast;
+        // $cast->nama = $request->input('name_cast');
+        // $cast->umur = $request->input('age_cast');
+        // $cast->bio = $request->input('bio_cast');
+        // $cast->save();
+
+        Cast::storeCast();
         return redirect('/cast')->with(['success' => 'Data Berhasil Disimpan!']);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, $cast_id)
+    public function update($cast_id)
     {
-        $cast = Cast::findOrFail($cast_id);
-        $cast->update([
-            'nama'     => $request->name_cast,
-            'umur'   => $request->age_cast,
-            'bio'   => $request->bio_cast
-        ]);
+        // $cast = Cast::findOrFail($cast_id);
+        // $cast->update([
+        //     'nama'     => $request->name_cast,
+        //     'umur'   => $request->age_cast,
+        //     'bio'   => $request->bio_cast
+        // ]);
+        Cast::updateCast($cast_id);
         return redirect('/cast')->with(['success' => 'Data Berhasil Diupdate!']);
     }
 
@@ -51,7 +56,9 @@ class CastController extends Controller
      */
     public function destroy($cast_id)
     {
-        $query = Cast::findOrFail($cast_id)->delete();
+        // Cast::findOrFail($cast_id)->delete();
+
+        Cast::destroyCast($cast_id);
         return redirect('/cast')->with(['success' => 'Data Berhasil Dihapus!']);
     }
 }
