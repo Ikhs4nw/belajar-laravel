0fd02040e9a5f59c380ca183137f25b8fb2bb666
Ikhsan Wanugrah <ikhsanwanugrah93@gmail.com>
2024-01-24 18:40:48 +0700

    Minor Fix In Route File

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,7 +17,11 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+// //route resource
+// Route::resource('/', ::class);
+// Route::resource('/home', \App\Http\Controllers\HomeController::class);
+// Route::resource('/cast', \App\Http\Controllers\CastController::class);
+
 
 /*
 Route::get('/register', [AuthController::class, 'register']);
@@ -24,16 +29,25 @@ Route::get('/register', [AuthController::class, 'register']);
 Route::get('/welcome', [AuthController::class, 'welcome']);
 
 Route::post('/welcome', [AuthController::class, 'postData']);
+
 */
 
+Route::get('/', [HomeController::class, 'index']);
 Route::get('/dashboard', [HomeController::class, 'dashboard']);
-
-// Route::get('/table', [AuthController::class, 'table']);
-
-// Route::get('/data-tables', [AuthController::class, 'data_table']);
-
 Route::get('/cast', [CastController::class, 'index']);
-Route::post('/cast/save', [CastController::class, 'store']);
-Route::post('/cast/{cast_id}/update', [CastController::class, 'update']);
+Route::post('/cast', [CastController::class, 'store']);
+Route::post('/cast/{cast_id}', [CastController::class, 'update']);
 Route::get('/cast/{cast_id}/destroy', [CastController::class, 'destroy']);
+
+
+// Route::get('/dashboard', [HomeController::class, 'dashboard']);
+
+// // Route::get('/table', [AuthController::class, 'table']);
+
+// // Route::get('/data-tables', [AuthController::class, 'data_table']);
+
+// Route::get('/cast', [CastController::class, 'index']);
+// Route::post('/cast/save', 'CastController@store');
+// Route::post('/cast/{cast_id}/update', [CastController::class, 'update']);
+// Route::get('/cast/{cast_id}/destroy', [CastController::class, 'destroy']);
 
