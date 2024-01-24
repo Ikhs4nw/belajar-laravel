5e9ee50980712166b4fa7da1bb3aaaeae2785b8d
Ikhsan Wanugrah <ikhsanwanugrah93@gmail.com>
2024-01-25 00:12:30 +0700

    Proses Dd Dipindahkan Dari Class Ke Model

--- app/Models/Cast.php
+++ app/Models/Cast.php
@@ -4,15 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Cast extends Model
 {
     use HasFactory;
-    // protected $fillable = [
-    //     'nama',
-    //     'umur',
-    //     'bio',
-    // ];cd 
-
     protected $guarded = ['id'];
+    public static function getCast(){
+        $cast = DB::table('casts')->get();
+        return $cast;
+    }
+    public static function storeCast(){
+        $nama = request()->name_cast;
+        $umur = request()->age_cast;
+        $bio = request()->bio_cast;
+
+        DB::table('casts')->insert([
+            ['nama' => $nama, 'umur' => $umur, 'bio' => $bio]
+        ]);
+    }
+    public static function updateCast($cast_id){
+        $nama = request()->name_cast;
+        $umur = request()->age_cast;
+        $bio = request()->bio_cast;
+
+        DB::table('casts')
+          ->where('id', $cast_id)
+          ->update(['nama' => $nama,'umur' => $umur, 'bio' => $bio]);
+    }
+    public static function destroyCast($cast_id){
+        DB::table('casts')->where('id', $cast_id)->delete();
+    }
 }
